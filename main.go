d04f3c51650ff7bad654b29980a5cae6fcd58dd7
ayou <youxingzhi@qq.com>
2018-02-06 23:23:10 +0800

    gin test; data output middleware test

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"goBuy/config"
+	"fmt"
+)
 
+func main() {
+	config.Load()
+
+	r := gin.New()
+
+	// data output middleware
+	r.Use(func(c *gin.Context) {
+		c.Next()
+
+		data, _ := c.Get("data")
+		status := c.Writer.Status()
+
+		if status == 200 {
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg": "ok",
+				"data": data,
+			})
+		} else {
+			c.JSON(status, gin.H{
+				"code": -1,
+				"msg": c.Err(),
+			})
+		}
+	})
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.GET("/test", func(c *gin.Context) {
+		c.Set("data", gin.H{"name": "youxingzhi"})
+	})
+
+	r.Use(func(context *gin.Context) {
+		fmt.Println("This will not execute if path (/test) match successfully")
+	})
+
+	r.Run(":" + config.Conf.Port)
 }
